3c2b5fe9be8386fa8b1e1b1f41a59e07b6e3eabd
Rachel Knight <dinkins.rachel@gmail.com>
2015-04-04 18:44:55 -0400

    Added scroll featues js and removed nav animation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-(function($) {          
-    $(document).ready(function(){                    
-        $(window).scroll(function(){                          
-            if ($(this).scrollTop() > 500) {
-                $('nav').fadeIn(500);
-            } else {
-                $('nav').fadeOut(500);
-            }
-        });
-    });
-})(jQuery);
+$(document).ready(function(){
+
+
+});
