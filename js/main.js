a05b62dd51fa99fbdfe7450ee216734bf5ad6552
Rachel Knight <dinkins.rachel@gmail.com>
2015-04-04 19:42:02 -0400

    Added sticky nav plugin

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-$(document).ready(function(){
-
-
-});
+      jQuery(function($) {
+        $(document).ready( function() {
+          $('.navbar-wrapper').stickUp({
+                        parts: {
+                          0:'education',
+                          1:'work',
+                          2: 'skills',
+                          3: 'interests',
+                        },
+                        itemClass: 'menuItem',
+                        itemHover: 'active',
+                        topMargin: 'auto'
+                      });
+        });
+      });
