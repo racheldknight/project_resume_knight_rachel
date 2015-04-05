8d9cc7649ff309a5fc077df551dc9d48155242e1
Rachel Knight <dinkins.rachel@gmail.com>
2015-04-05 11:36:42 -0400

    Added graph js plugin for skills

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
-      jQuery(function($) {
-        $(document).ready( function() {
-          $('.navbar-wrapper').stickUp({
-                        parts: {
-                          0: 'education',
-                          1: 'work',
-                          2: 'skills',
-                          3: 'interests',
-                        },
-                        itemClass: 'menuItem',
-                        itemHover: 'active',
-                        topMargin: 'auto'
-                      });
+arrayOfData = new Array(
+    [90,'HTML','#F1F2E4'],
+    [90,'CSS','#F1F2E4'],
+    [70,'SEO','#F1F2E4'],
+    [60,'jQuery/JS','#F1F2E4'],
+    [80,'Branding','#F1F2E4'],
+    [90, 'Design', '#F1F2E4']
+);
+
+jQuery(function($) {
+    $(document).ready( function() {
+        $('.navbar-wrapper').stickUp({
+            parts: {
+                0: 'education',
+                1: 'work',
+                2: 'skills',
+                3: 'interests',
+            },
+            itemClass: 'menuItem',
+            itemHover: 'active',
+            topMargin: 'auto'
         });
-      });
+
+        $('#graph').jqBarGraph({ 
+            data: arrayOfData,
+            showValues: false,
+        });
+
+    });
+
+});
